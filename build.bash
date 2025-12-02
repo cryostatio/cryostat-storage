36dc2f8e4dfdb00ffdb8737e44b1937583ec0fb9
Andrew Azores <me@andrewazor.es>
2025-12-02 11:57:13 -0500

    build(versions): use path relative to build script (#64)

--- build.bash
+++ build.bash
@@ -6,6 +6,6 @@ DIR="$(dirname "$(readlink -f "$0")")"
 
 BUILDER="${BUILDER:-podman}"
 
-source versions.env
+source "${DIR}/versions.env"
 
 ${BUILDER} build "${DIR}" -f "${DIR}/Dockerfile" --build-arg ref="${SEAWEED_VERSION:-master}" -t quay.io/cryostat/cryostat-storage:latest
